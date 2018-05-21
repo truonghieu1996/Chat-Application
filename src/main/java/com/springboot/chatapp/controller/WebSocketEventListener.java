cdf1c87cd0219fef96a353d979a3da85156b8e4a
Truong Hieu <tmhieu15th@gmail.com>
2018-05-21 13:30:03 +0700

    Add Web socket event listener.

---
+++ src/main/java/com/springboot/chatapp/controller/WebSocketEventListener.java
@@ -0,0 +1,11 @@
+package com.springboot.chatapp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebSocketEventListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
+}

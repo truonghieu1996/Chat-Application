40aa77c3a20e125aad835d1a9e5653737e9997f3
Hieu <tmhieu15th@gmail.com>
2018-05-21 07:14:22 +0700

    Init project.

---
+++ src/main/java/com/springboot/chatapp/ChatAppApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAppApplication.class, args);
+	}
+}

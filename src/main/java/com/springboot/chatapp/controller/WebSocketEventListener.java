97e45e605df14006f75ecaf39c00472e28fea7d5
Truong Hieu <tmhieu15th@gmail.com>
2018-05-21 13:52:57 +0700

    Update Web socket event listener.

--- src/main/java/com/springboot/chatapp/controller/WebSocketEventListener.java
+++ src/main/java/com/springboot/chatapp/controller/WebSocketEventListener.java
@@ -1,11 +1,35 @@
 package com.springboot.chatapp.controller;
 
+import com.springboot.chatapp.model.ChatMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Component
 public class WebSocketEventListener {
 
     private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
+
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+
+    public void handleWebSocketDisconnectListener( SessionDisconnectEvent event ) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if( username != null ) {
+            logger.info("User Disconnected : "+ username);
+
+            ChatMessage chatMessage = new ChatMessage();
+            chatMessage.setType( ChatMessage.MessageType.LEAVE );
+            chatMessage.setSender(username);
+
+            messagingTemplate.convertAndSend("/topic/public", chatMessage);
+        }
+    }
 }

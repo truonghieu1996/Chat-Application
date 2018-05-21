23fb0b381976a1db75e2af4dc99b5e95388bcf8c
Hieu <tmhieu15th@gmail.com>
2018-05-21 07:52:38 +0700

    Update model.

--- src/main/java/com/springboot/chatapp/model/ChatMessage.java
+++ src/main/java/com/springboot/chatapp/model/ChatMessage.java
@@ -1,5 +1,7 @@
 package com.springboot.chatapp.model;
 
+import java.util.Objects;
+
 public class ChatMessage {
 
     private MessageType type;
@@ -10,6 +12,14 @@ public class ChatMessage {
         CHAT,
         JOIN,
         LEAVE
+    }
+
+    public ChatMessage() {}
+
+    public ChatMessage(MessageType type, String content, String sender) {
+        this.type = type;
+        this.content = content;
+        this.sender = sender;
     }
 
     public MessageType getType() {
@@ -36,4 +46,28 @@ public class ChatMessage {
         this.sender = sender;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChatMessage that = (ChatMessage) o;
+        return type == that.type &&
+                Objects.equals(content, that.content) &&
+                Objects.equals(sender, that.sender);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(type, content, sender);
+    }
+
+    @Override
+    public String toString() {
+        return "ChatMessage{" +
+                "type=" + type +
+                ", content='" + content + '\'' +
+                ", sender='" + sender + '\'' +
+                '}';
+    }
 }
